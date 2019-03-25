07c3774e9a48ef0811822dae693c5e295755f2be
Luã Enrique Zangrande <lua.enriquezan@gmail.com>
2019-03-25 00:16:16 -0300

    adding the class to hash the file with sha 256

---
+++ HashCalculator/hashFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace HashCalculator
+{
+    class hashFile
+    {
+        private SHA256 sha256;
+
+
+        private byte[] HashYourFileWithSha256(string file)
+        {
+            sha256 = SHA256.Create();
+            using (FileStream stream = File.OpenRead(file)) { 
+                return sha256.ComputeHash(stream);
+            }
+        }
+
+
+    }
+}
