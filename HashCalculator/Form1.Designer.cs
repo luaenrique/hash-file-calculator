0a64315d7d7f1a6caed266f8dbbc92fa40f0faea
Luã Enrique Zangrande <lua.enriquezan@gmail.com>
2019-03-24 23:52:59 -0300

    basic interface creation

--- HashCalculator/Form1.Designer.cs
+++ HashCalculator/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pathFile1 = new System.Windows.Forms.TextBox();
+            this.btnFile1 = new System.Windows.Forms.Button();
+            this.hashFile1 = new System.Windows.Forms.TextBox();
+            this.hashFile2 = new System.Windows.Forms.TextBox();
+            this.btnFile2 = new System.Windows.Forms.Button();
+            this.pathFile2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pathFile1
+            // 
+            this.pathFile1.Location = new System.Drawing.Point(30, 63);
+            this.pathFile1.Name = "pathFile1";
+            this.pathFile1.Size = new System.Drawing.Size(319, 20);
+            this.pathFile1.TabIndex = 0;
+            this.pathFile1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnFile1
+            // 
+            this.btnFile1.Location = new System.Drawing.Point(346, 62);
+            this.btnFile1.Name = "btnFile1";
+            this.btnFile1.Size = new System.Drawing.Size(50, 21);
+            this.btnFile1.TabIndex = 1;
+            this.btnFile1.Text = "...";
+            this.btnFile1.UseVisualStyleBackColor = true;
+            this.btnFile1.Click += new System.EventHandler(this.btnFile1_Click);
+            // 
+            // hashFile1
+            // 
+            this.hashFile1.Location = new System.Drawing.Point(30, 122);
+            this.hashFile1.Multiline = true;
+            this.hashFile1.Name = "hashFile1";
+            this.hashFile1.Size = new System.Drawing.Size(366, 76);
+            this.hashFile1.TabIndex = 2;
+            // 
+            // hashFile2
+            // 
+            this.hashFile2.Location = new System.Drawing.Point(411, 122);
+            this.hashFile2.Multiline = true;
+            this.hashFile2.Name = "hashFile2";
+            this.hashFile2.Size = new System.Drawing.Size(366, 76);
+            this.hashFile2.TabIndex = 5;
+            // 
+            // btnFile2
+            // 
+            this.btnFile2.Location = new System.Drawing.Point(727, 62);
+            this.btnFile2.Name = "btnFile2";
+            this.btnFile2.Size = new System.Drawing.Size(50, 21);
+            this.btnFile2.TabIndex = 4;
+            this.btnFile2.Text = "...";
+            this.btnFile2.UseVisualStyleBackColor = true;
+            this.btnFile2.Click += new System.EventHandler(this.btnFile2_Click);
+            // 
+            // pathFile2
+            // 
+            this.pathFile2.Location = new System.Drawing.Point(411, 63);
+            this.pathFile2.Name = "pathFile2";
+            this.pathFile2.Size = new System.Drawing.Size(319, 20);
+            this.pathFile2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "File 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(411, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "File 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "SHA-256";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(411, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "SHA-256";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(30, 204);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(747, 50);
+            this.calcButton.TabIndex = 10;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(795, 338);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hashFile2);
+            this.Controls.Add(this.btnFile2);
+            this.Controls.Add(this.pathFile2);
+            this.Controls.Add(this.hashFile1);
+            this.Controls.Add(this.btnFile1);
+            this.Controls.Add(this.pathFile1);
+            this.Name = "Form1";
+            this.Text = "Hash Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox pathFile1;
+        private System.Windows.Forms.Button btnFile1;
+        private System.Windows.Forms.TextBox hashFile1;
+        private System.Windows.Forms.TextBox hashFile2;
+        private System.Windows.Forms.Button btnFile2;
+        private System.Windows.Forms.TextBox pathFile2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button calcButton;
     }
 }
 
--- HashCalculator/Form1.cs
+++ HashCalculator/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace HashCalculator
 {
@@ -15,6 +17,31 @@ namespace HashCalculator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFile1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = pathFile1.Text;
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathFile1.Text = openFileDialog1.FileName;
+                hashFile1.Clear();
+            }
+        }
+
+        private void btnFile2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = pathFile2.Text;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathFile2.Text = openFileDialog1.FileName;
+                hashFile2.Clear();
+            }
         }
     }
 }
